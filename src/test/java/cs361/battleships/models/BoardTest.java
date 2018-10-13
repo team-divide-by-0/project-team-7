7a5441837093758db3b5cd00a4648c7c6255a0f4
hillkai <hillkai@oregonstate.edu>
2018-10-13 12:41:32 -0700

    added tests for hit, miss, sunk, and surrender

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -1,7 +1,9 @@
 package cs361.battleships.models;
 
 import org.junit.Test;
+import static cs361.battleships.models.AtackStatus.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -21,5 +23,45 @@ public class BoardTest {
         assertTrue(board.placeShip(new Ship("MINESWEEPER"), 4, 'B', false));
         assertTrue(board.placeShip(new Ship("DESTROYER"), 9, 'G', true));
         assertTrue(board.placeShip(new Ship("BATTLESHIP"), 0, 'A'), false);
+    }
+
+    @Test
+    public void testHit(){
+        Board board = new Board();
+        Ship ship = new Ship("DESTROYER");
+        board.placeShip(ship, 1, 'A', false);
+        Result res = board.attack(1, 'A');
+        assertEquals(HIT, res.getResult());
+    }
+
+    @Test
+    public void testMiss(){
+        Board board = new Board();
+        Ship ship = new Ship("DESTROYER");
+        board.placeShip(ship, 1, 'A', false);
+        Result res = board.attack(1, 'D');
+        assertEquals(MISS, res.getResult());
+    }
+
+    @Test
+    public void testSunk(){
+        Board board = new Board();
+        Ship ship = new Ship("MINESWEEPER");
+        Ship ship2 = new Ship("MINESWEEPER");
+        board.placeShip(ship, 1, 'A', false);
+        board.placeShip(ship2, 2, 'A', false);
+        board.attack(1, 'A');
+        Result res = board.attack(1, 'B');
+        assertEquals(SUNK, res.getResult());
+    }
+
+    @Test
+    public void testSurrender(){
+        Board board = new Board();
+        Ship ship = new Ship("MINESWEEPER");
+        board.placeShip(ship, 1, 'A', false);
+        board.attack(1, 'A');
+        Result res = board.attack(1, 'B');
+        assertEquals(SURRENDER, res.getResult());
     }
 }
